824869160a20965fd4e2e489756a4a51c06da0a9
Yogesh Shelke <yogesh.shelke@fielmann.com>
2022-02-05 22:05:55 +0100

    feat:adding folder nvim

---
+++ nvim/lua/lualine-config/init.lua
@@ -0,0 +1,24 @@
+require'lualine'.setup({
+    options = {
+        --section_separators = { left = '', right = ''}, 
+        --component_separators = '',
+        icons_enabled = true,
+        theme = 'nord'
+    },
+     sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+     }
+})
